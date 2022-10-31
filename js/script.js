c3b8b674f0813ff82c0d7bbc2756c687492e20b5
Smael Nicolas <nico.smael@gmail.com>
2022-10-31 18:23:06 -0300

    import

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import { carouselDownloadStart } from "./carouselDownloads.js";
+import { startCarouselOneImage } from "./carouselOneImage.js";
 import { initalizeEventListeners } from "./gridImages.js";
 
 carouselDownloadStart();
+startCarouselOneImage();
 initalizeEventListeners();
