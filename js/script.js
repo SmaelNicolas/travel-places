000d95ba9eb3bc9123075412eefec1a2bb4f6426
Smael Nicolas <nico.smael@gmail.com>
2022-11-12 17:37:33 -0300

    comment import

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // import { handleSuggestionsCountrys } from "./handleSuggestionsCountrys.js";
 import { handleSuggestionsCountrys2 } from "./handleSuggestionsCountrys2.js";
-import { initializeAllSections } from "./initializeAllSectionsByCountry.js";
+// import { initializeAllSections } from "./initializeAllSectionsByCountry.js";
 import { section_6_start } from "./section6.js";
 
 handleSuggestionsCountrys2();
