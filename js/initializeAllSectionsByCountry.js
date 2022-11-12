a6936cefd04c46d8081f893dfcd4af7b914d3366
Smael Nicolas <nico.smael@gmail.com>
2022-11-12 00:42:41 -0300

    initialize all scripts

---
+++ js/initializeAllSectionsByCountry.js
@@ -0,0 +1,15 @@
+import { section_1_start } from "./section1.js";
+import { section_2_start } from "./section2.js";
+import { section_3_start } from "./section3.js";
+import { section_4_Start } from "./section4.js";
+import { section_5_start } from "./section5.js";
+import { section_6_start } from "./section6.js";
+
+export const initializeAllSections = (arrayCountries) => {
+	section_1_start();
+	section_2_start();
+	section_3_start();
+	section_4_Start();
+	section_5_start();
+	section_6_start();
+};
