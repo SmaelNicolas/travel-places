6ed42bda673181ef1dc0648b87ad48b3b488890e
Smael Nicolas <nico.smael@gmail.com>
2022-11-12 20:47:37 -0300

    move logic to handleAll js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,5 @@
 // import { handleSuggestionsCountrys } from "./handleSuggestionsCountrys.js";
 import { handleSuggestionsCountrys2 } from "./handleSuggestionsCountrys2.js";
-// import { initializeAllSections } from "./initializeAllSectionsByCountry.js";
 import { section_6_start } from "./section6.js";
 
 handleSuggestionsCountrys2();
-
-// fetch("../data/countries.json")
-// 	.then((resp) => resp.json())
-// 	.then((res) => initializeAllSections(res));
-
-window.addEventListener("resize", () => {
-	let windowWidth = window.innerWidth;
-	if (windowWidth >= 920) {
-		section_6_start();
-	}
-	if (windowWidth >= 725 && windowWidth < 920) {
-		section_6_start();
-	}
-	if (windowWidth < 725) {
-		section_6_start();
-	}
-});
