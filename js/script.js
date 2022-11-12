c491dbb8a29d3c6b3eb672498fd533184a013726
Smael Nicolas <nico.smael@gmail.com>
2022-11-12 00:42:22 -0300

    remove imports to another fn

--- js/script.js
+++ js/script.js
@@ -1,20 +1,12 @@
 import { handleSuggestionsCountrys } from "./handleSuggestionsCountrys.js";
-import { section_1_start } from "./section1.js";
-import { section_2_start } from "./section2.js";
-import { section_3_start } from "./section3.js";
-import { section_5_start } from "./section5.js";
+import { initializeAllSections } from "./initializeAllSectionsByCountry.js";
 import { section_6_start } from "./section6.js";
 
-section_1_start();
-section_2_start();
-section_3_start();
-section_5_start();
-section_6_start();
 handleSuggestionsCountrys();
 
 fetch("../data/countries.json")
 	.then((resp) => resp.json())
-	.then((res) => console.log(res));
+	.then((res) => initializeAllSections(res));
 
 window.addEventListener("resize", () => {
 	let windowWidth = window.innerWidth;
