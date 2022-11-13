fc44462d51ca12bc7240191452bd0073731f1cca
Smael Nicolas <nico.smael@gmail.com>
2022-11-13 18:19:36 -0300

    dinamic render section 6

--- js/initializeAllSectionsByCountry.js
+++ js/initializeAllSectionsByCountry.js
@@ -18,13 +18,25 @@ export const initializeAllSections = (infoCountry) => {
 		section_5_start(country.section_5, country.country, country.styles);
 		let windowWidth = window.innerWidth;
 		if (windowWidth >= 920) {
-			section_6_start(infoCountry[0].country, country.styles);
+			section_6_start(
+				country.section_6,
+				infoCountry[0].country,
+				country.styles
+			);
 		}
 		if (windowWidth >= 725 && windowWidth < 920) {
-			section_6_start(infoCountry[0].country, country.styles);
+			section_6_start(
+				country.section_6,
+				infoCountry[0].country,
+				country.styles
+			);
 		}
 		if (windowWidth < 725) {
-			section_6_start(infoCountry[0].country, country.styles);
+			section_6_start(
+				country.section_6,
+				infoCountry[0].country,
+				country.styles
+			);
 		}
 	});
 };
