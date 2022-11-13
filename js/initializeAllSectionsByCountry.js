896abc61f1383c2f2c7dc67d466bef032a4b8789
Smael Nicolas <nico.smael@gmail.com>
2022-11-13 16:58:11 -0300

    pass styles via props

--- js/initializeAllSectionsByCountry.js
+++ js/initializeAllSectionsByCountry.js
@@ -11,20 +11,20 @@ export const initializeAllSections = (infoCountry) => {
 	);
 	containerTravelPlaces.innerHTML = "";
 	infoCountry.map((country) => {
-		section_1_start(country.section_1, country.country);
+		section_1_start(country.section_1, country.country, country.styles);
 		section_2_start(country.section_2, country.country, country.styles);
-		section_3_start(country.section_3, country.country);
-		section_4_Start(country.section_4, country.country);
-		section_5_start(country.section_5, country.country);
+		section_3_start(country.section_3, country.country, country.styles);
+		section_4_Start(country.section_4, country.country, country.styles);
+		section_5_start(country.section_5, country.country, country.styles);
 		let windowWidth = window.innerWidth;
 		if (windowWidth >= 920) {
-			section_6_start(infoCountry[0].country);
+			section_6_start(infoCountry[0].country, country.styles);
 		}
 		if (windowWidth >= 725 && windowWidth < 920) {
-			section_6_start(infoCountry[0].country);
+			section_6_start(infoCountry[0].country, country.styles);
 		}
 		if (windowWidth < 725) {
-			section_6_start(infoCountry[0].country);
+			section_6_start(infoCountry[0].country, country.styles);
 		}
 	});
 };
